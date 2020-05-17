e4a2793318abf33970a88501d7fee21d46d25d49
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-05-18 01:10:12 +0530

    ADDED : Test To Check Given 1 Inch And 1 Feet Should Return Not Equal

--- src/test/java/com/quantitymeasurement/test/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurement/test/QuantityMeasurementTest.java
@@ -91,4 +91,12 @@ public class QuantityMeasurementTest {
         quantityMeasurement.convert(quantityMeasurement1);
         Assert.assertNotEquals(quantityMeasurement, quantityMeasurement1);
     }
+
+    @Test
+    public void givenInchAndFeet_When1and1_ShouldReturnNotEqual() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Units.INCH, 1);
+        QuantityMeasurement quantityMeasurement1 = new QuantityMeasurement(Units.FEET, 1);
+        quantityMeasurement.convert(quantityMeasurement1);
+        Assert.assertNotEquals(quantityMeasurement, quantityMeasurement1);
+    }
 }
